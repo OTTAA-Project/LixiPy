ce61670674919d3cf05f3ed3da0a11f4a1502bdc
lopezjuanma96 <lopezjuanma96@gmail.com>
2021-09-07 11:05:43 -0300

    Used the time part of the fast feature matrix processing from freqs to create a times feature matrix generator.

--- lixipy/freqs.py
+++ lixipy/freqs.py
@@ -356,39 +356,11 @@ def fast_feature_matrix_gen(signal, sample_rate, labels, startpoint_timestamps, 
         - label_matrix(np.array): matrix with all the labels.
         
     """
-    total_dataloss = 0
-    fv_matrix_list = []
-    labels_matrix_list = []
-    for i in range(len(labels)):
-      each_label_matrix_size = 0
-      for t in startpoint_timestamps[i]:
-        vector = signal[t+stim_delay:t+stim_size]
-        end_list_for_dataloss = []
-        vector_reshaped_list = []
-        final_shape = 0
-        for start in range(0, window_size, stride):
-          quant = (vector.shape[0]-start)//window_size
-          end = quant*window_size+start
-          end_list_for_dataloss.append(end)
-          vector_reshaped = vector[start:end].reshape(quant, window_size)
-          #print("Vector reshaped:\n", vector_reshaped)
-          final_shape += vector_reshaped.shape[0]
-          vector_reshaped_list.append(vector_reshaped.copy())
-        #print("dataloss: ",  vector.shape[0] - np.array(end_list_for_dataloss).max())
-        total_dataloss += vector.shape[0] - np.array(end_list_for_dataloss).max()
-        final_reshaped_vector = np.empty((final_shape, window_size))
-        temp = len(vector_reshaped_list)
-        for n, v in enumerate(vector_reshaped_list):
-          final_reshaped_vector[n:n+v.shape[0]*temp:temp,:] = v
-        #print(final_reshaped_vector)
-        fv_matrix_list.append(final_reshaped_vector.copy())
-        each_label_matrix_size += final_reshaped_vector.shape[0]
-      temp_labels = np.zeros((each_label_matrix_size, len(labels)))
-      temp_labels[:, i] = 1.0
-      labels_matrix_list.append(temp_labels)
+    time_fv_matrix, onehot_labels_matrix = times.fast_feature_matrix_gen(signal, labels, startpoint_timestamps, 
+                                                                        stim_size, stim_delay, stride, filter_window, 
+                                                                        window_size, print_data_loss)
 
-    fft_bins, fft_matrix = fft_calc(np.concatenate(fv_matrix_list, axis=0), sample_rate, norm, filter_window)
-    onehot_labels_matrix = np.concatenate(labels_matrix_list, axis=0)
+    fft_bins, fft_matrix = fft_calc(time_fv_matrix, sample_rate, norm, filter_window)
     
     if plot_average:
       plt.figure()
--- lixipy/times.py
+++ lixipy/times.py
@@ -8,3 +8,60 @@ else:
     from lixipy import ops
     from lixipy import freqs
     from lixipy import intels
+
+import numpy as np
+
+def fast_feature_matrix_gen(signal, labels, startpoint_timestamps,
+                            stim_size, stim_delay, stride, 
+                            filter_window = None, window_size = 512, 
+                            print_data_loss = True
+                            ):
+
+    """
+    Description:
+        This method is faster than the safe_feature_matrix_gen, but may alter data with a stride that is not an even divisor of the window size.
+    
+    Inputs:
+        See doc for feature_matrix_gen. Only difference lies in that every time interval or vector size is only passed in sample measure, use feature_matrix_gen with method="fast" to pass it in seconds.
+        
+    Outputs:
+        - fv_matrix(np.array): matrix full of feature vector
+        - label_matrix(np.array): matrix with all the labels.
+
+    """
+
+    total_dataloss = 0
+    fv_matrix_list = []
+    labels_matrix_list = []
+    for i in range(len(labels)):
+      each_label_matrix_size = 0
+      for t in startpoint_timestamps[i]:
+        vector = signal[t+stim_delay:t+stim_size]
+        end_list_for_dataloss = []
+        vector_reshaped_list = []
+        final_shape = 0
+        for start in range(0, window_size, stride):
+          quant = (vector.shape[0]-start)//window_size
+          end = quant*window_size+start
+          end_list_for_dataloss.append(end)
+          vector_reshaped = vector[start:end].reshape(quant, window_size)
+          #print("Vector reshaped:\n", vector_reshaped)
+          final_shape += vector_reshaped.shape[0]
+          vector_reshaped_list.append(vector_reshaped.copy())
+        #print("dataloss: ",  vector.shape[0] - np.array(end_list_for_dataloss).max())
+        total_dataloss += vector.shape[0] - np.array(end_list_for_dataloss).max()
+        final_reshaped_vector = np.empty((final_shape, window_size))
+        temp = len(vector_reshaped_list)
+        for n, v in enumerate(vector_reshaped_list):
+          final_reshaped_vector[n:n+v.shape[0]*temp:temp,:] = v
+        #print(final_reshaped_vector)
+        fv_matrix_list.append(final_reshaped_vector.copy())
+        each_label_matrix_size += final_reshaped_vector.shape[0]
+      temp_labels = np.zeros((each_label_matrix_size, len(labels)))
+      temp_labels[:, i] = 1.0
+      labels_matrix_list.append(temp_labels)
+
+    fv_matrix = np.concatenate(fv_matrix_list, axis=0)
+    onehot_labels_matrix = np.concatenate(labels_matrix_list, axis=0)
+    
+    return fv_matrix, onehot_labels_matrix
